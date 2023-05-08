04597fb2dbdf66c9f89ba739d34c6299a4b4ba33
jinkyo.park <jinkyo.park@brique.co.kr>
2023-05-08 13:13:27 +0900

    망치, 열쇠 사용 함수 제작

--- maps/useItem.go
+++ maps/useItem.go
@@ -10,29 +10,73 @@ func UseItem(selectedCommand string, ableCommandsString string, nowX int, nowY i
 		return false
 	}
 
+	directionRoom := [4]*mapObjects.Room{east, west, south, north}
+
 	for _, item := range *myItems {
 		if !strings.Contains(selectedCommand, item) {
 			continue
 		}
 
-		if item == "망치" && east.DoorType == 2 {
-			east.DoorType = 0
-			east.Name = "🔳"
-
-			if len(*myItems) == 1 && (*myItems)[0] == item {
-				*myItems = []string{}
-			} else {
-				for i := 0; i < len(*myItems); i++ {
-					if (*myItems)[i] == item {
-						*myItems = append((*myItems)[:i], (*myItems)[i+1])
-						break
-					}
-				}
+		for _, room := range directionRoom {
+			isUsed := useHammer(item, room, myItems)
+			if isUsed {
+				return true
 			}
-
-			return true
 		}
+
+		for _, room := range directionRoom {
+			isUsed := useKey(item, room, myItems)
+			if isUsed {
+				return true
+			}
+		}
+
 	}
 
 	return false
+}
+
+func removeItem(myItems *[]string, item string) {
+	if len(*myItems) == 1 && (*myItems)[0] == item {
+		*myItems = []string{}
+	} else {
+		for i, myItem := range *myItems {
+			if myItem == item {
+				*myItems = append((*myItems)[:i], (*myItems)[i+1])
+				break
+			}
+		}
+	}
+}
+
+func useHammer(item string, room *mapObjects.Room, myItems *[]string) bool {
+	if item != "망치" {
+		return false
+	}
+
+	if room.DoorType != 2 {
+		return false
+	}
+
+	room.DoorType = 0
+	room.Name = "🔳"
+
+	removeItem(myItems, item)
+	return true
+}
+
+func useKey(item string, room *mapObjects.Room, myItems *[]string) bool {
+	if item != "열쇠" {
+		return false
+	}
+
+	if room.DoorType != 3 {
+		return false
+	}
+
+	room.DoorType = 0
+	room.Name = "🔳"
+
+	removeItem(myItems, item)
+	return true
 }
