53e1e76f9f20ecb460569a15bd2427f0d2c47fc2
jinkyo.park <jinkyo.park@brique.co.kr>
2023-05-08 14:56:15 +0900

    방 및 아이템 아이콘 상수화

--- types/doorTypes.go
+++ types/doorTypes.go
@@ -1,10 +1,19 @@
 package types
 
 type DoorTypes int
+type DoorNames string
 
 const (
 	NoDoor DoorTypes = iota
 	Wood
 	Glass
 	Locked
+)
+
+const (
+	NoDoorName DoorNames = "🔳"
+	WoodName   DoorNames = "🚪"
+	GlassName  DoorNames = "🧊"
+	LockedName DoorNames = "🔒"
+	WallName   DoorNames = "🚧"
 )
